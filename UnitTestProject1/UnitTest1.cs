80de0dbb626de027e3f5db1415b01ba71190338d
vuongkimphat97 <vuongkimphat1997@gmail.com>
2019-09-20 11:43:36 +0700

    test2

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BT1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private int a,b,c,d;
+        [TestInitialize]
+        public void testmethod1()
+        {
+            this.a = 10;
+            this.b = 5;
+            this.c = -6;
+            this.d = -11;
+        }
+        [TestMethod]
+        public void testconghaisoduong()
+        {
+            maytinh call = new maytinh(this.a,this.b);
+            int expected = 15;
+            int actual = call.execute("+");
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void testconghaisoam()
+        {
+            maytinh call = new maytinh(this.c,this.d);
+            int expected = -17;
+            int actual = call.execute("+");
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
